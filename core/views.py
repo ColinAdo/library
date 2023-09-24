200a97067bc97e07da204cdf4e759ca0525bda41
ColinAdo <colinadodev30@gmail.com>
2023-09-24 16:24:15 +0300

    Add books list

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
 def home(request):
     template = 'core/index.html'
+    books = Book.objects.all().order_by('-date_posted')
     context = {
-
+        'books': books
     }
     return render(request, template, context=context)
