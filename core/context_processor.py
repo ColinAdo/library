7495ea17a3c87dbab888c2ac4c7e3ce583a9bad8
ColinAdo <colinadodev30@gmail.com>
2023-09-26 23:27:37 +0300

    Update context_processor.py

--- core/context_processor.py
+++ core/context_processor.py
@@ -2,8 +2,13 @@ from .models import Category
 from userAuths.models import CustomUser
 
 def default(request):
-    categories = Category.objects.all()
-    p = CustomUser.objects.get(username=request.user.username)
+    p  = None
+    
+    try:
+        categories = Category.objects.all()
+        p = CustomUser.objects.get(username=request.user.username)
+    except:
+        None
 
     return {
         'categories': categories,
