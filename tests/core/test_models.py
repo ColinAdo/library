95385f6eccdd632979f8b31f996a3240027a1a67
ColinAdo <colinadodev30@gmail.com>
2023-12-14 07:57:08 +0300

    Update .gitignore

--- tests/core/test_models.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-pytestmark = pytest.mark.django_db
-
-class TestCategoryModel:
-    def test_str_return(self, category_factory):
-        category = category_factory(title="Test Category")
-        assert category.__str__() == "Test Category"
