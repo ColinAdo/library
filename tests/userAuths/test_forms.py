424cae82bc294801964cdfe12c8028f0a956130e
ColinAdo <colinadodev30@gmail.com>
2023-12-17 11:41:30 +0300

    Add reusable test code to userAuths forms

--- tests/userAuths/test_forms.py
+++ tests/userAuths/test_forms.py
@@ -5,23 +5,26 @@ from userAuths.forms import RegisterForm, ProfileFrom
 
 @pytest.mark.django_db
 class TestRegisterForm:
-    def test_register_form_valid(self):
-        data = {
+    def setup_method(self):
+        # Common data used in multiple test methods
+        self.valid_data = {
             'username': 'test',
             'email': 'test@example.com',
             'password1': 'testpassword',
             'password2': 'testpassword',
         }
 
-        form = RegisterForm(data=data)
-        assert form.is_valid()
-
-    def test_register_form_errors(self):
-        data = {
+        self.invalid_data = {
             'username': '',
             'email': 'x',
         }
-        form = RegisterForm(data=data)
+
+    def test_register_form_valid(self):
+        form = RegisterForm(data=self.valid_data)
+        assert form.is_valid()
+
+    def test_register_form_errors(self):
+        form = RegisterForm(data=self.invalid_data)
 
         assert 'username' in form.errors
         assert 'email' in form.errors
@@ -29,41 +32,39 @@ class TestRegisterForm:
         assert 'Enter a valid email address.' in form.errors['email']
 
     def test_password_not_match(self):
-        data = {
-            'username': 'test',
-            'email': 'test@example.com',
-            'password1': 'test',
-            'password2': 'testpassword',
-        }
+        self.valid_data['password2'] = "test"
 
-        form = RegisterForm(data=data)
-        assert form.data['password1'] != form.data['password2']
-        
+        form = RegisterForm(data=self.valid_data)
+        assert self.valid_data['password1'] != form.data['password2']
+
 @pytest.mark.django_db
 class TestProfileForm:
+    def setup_method(self):
+        self.valid_data = {
+            'profile_pic': 'profile',
+            'career': 'teacher',
+            'location': 'Nairobi, Kenya',
+        }
+
+        self.invalid_data = {
+            'career': 'Teacher',
+            'location': 'nairobi, Kenya',
+        }
+
     def test_profile_form_valid(self):
 
         """
         Here the data muct correspond to the actual choices defined in the models.py 
         for career and choices else the test will fail 
         """
-        data = {
-            'profile_pic': 'profile',
-            'career': 'teacher',
-            'location': 'Nairobi, Kenya',
-        }
 
-        form = ProfileFrom(data=data)
+        form = ProfileFrom(data=self.valid_data)
         if not form.is_valid():
             print(form.errors)
 
         assert form.is_valid()
 
     def test_invalid_choices(self):
-        data = {
-            'career': 'Teacher',
-            'location': 'nairobi, Kenya',
-        }
-        form = ProfileFrom(data=data)
+        form = ProfileFrom(data=self.invalid_data)
         assert 'career' in form.errors
         assert 'location' in form.errors
