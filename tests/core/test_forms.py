a7493ef9b20a8e6bc59f7b8270f8d4887b4ac787
ColinAdo <colinadodev30@gmail.com>
2023-12-17 11:44:52 +0300

    Add reusable test code to core forms

--- tests/core/test_forms.py
+++ tests/core/test_forms.py
@@ -1,15 +1,18 @@
 from core.forms import ReviewForm
 
 class TestForm:
+    def setup_method(self):
+        self.valid_data = {'comment': 'Test comment'}
+        self.invalid_data = data = {'comment': ''}
+        
     def test_form_valid(self):
-        data = {'comment': 'Test comment'}
-        form = ReviewForm(data=data)
+        form = ReviewForm(data=self.valid_data)
 
         assert form.is_valid()
 
     def test_form_errors(self):
-        data = {'comment': ''}
-        form = ReviewForm(data=data)
+        
+        form = ReviewForm(data=self.invalid_data)
 
         assert 'comment' in form.errors
         assert 'This field is required.' in form.errors['comment']
